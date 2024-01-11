1d3a4ae6f40477b57aabed40ade0ab03e9b80616
John <77732546+JumpingBrain@users.noreply.github.com>
2024-01-11 17:16:09 +0000

    importing 'MyWorld' code

--- scripts/data.py
+++ scripts/data.py
@@ -2,11 +2,22 @@ import pygame
 import os
 import json
 
+from pathlib import Path
+path = Path(os.getcwd())
+PARDIR = str(path.parent.absolute())
+
+pygame.font.init()
+
 
 class Data:
 	def __init__(self):
-		self.imagedir = 'images/'
-		self.jsondir = 'json/'
+		mon_info = pygame.display.Info()
+		self.mon_siz = [mon_info.current_w, mon_info.current_h]
+
+		self.jsondir = PARDIR + '/json/'
+		self.imagedir = PARDIR + '/images/'
+		self.fontdir = PARDIR + '/fonts/'
+		self.sounddir = PARDIR + '/sounds/'
 
 		self.images = {}
 		for foldername in os.listdir(self.imagedir):
@@ -17,39 +28,56 @@ class Data:
 				self.images[foldername][filename[:-dis]] = self.unpack(
 					pygame.image.load(curr_dir + filename), unpack_size)
 
+		#all surfaces must be saved here
+		#server cannot transfer surfaces
+		#so all surfaces must be saved on
+		#each client machine
+
 		self.p_images = {}
 		for key in self.images['player']:
 			images = self.images['player'][key]
 			self.p_images[f'{key} 1'] = [image for image in images] #right (1)
 			self.p_images[f'{key} -1'] = [pygame.transform.flip(image, True, False) for image in images] #left (-1)
-
 		self.running_ani_speed = .25
 		self.idle_ani_speed = .25
 		self.hitground_ani_speed = .2
+		self.render_dis = 400
+
+		#do the same with fonts as they're a surface
+		#(read above comments if unsure what i mean)
+		self.fonts = {}
+		f_base = 6
+		f_size = f_base
+		for font_name in os.listdir(self.fontdir):
+			for i in range(6):
+				font = pygame.font.Font(self.fontdir + font_name, f_size)
+				self.fonts[font_name[:-4] + str(f_size)] = font
+				f_size += 2
+			f_size = f_base
+
 
 		self.ui_dat = json.load(open('non_ingame_data.json'))
 
 		self.raw_map_data = {key[:-5]:json.load(open(self.jsondir + 'maps/' + key)) for key in os.listdir(self.jsondir + 'maps/')}
-		self.render_dis = 400
 
 	def get_unpack_size(self, image_name):
-			dis = 0
-			extracted = ''
-			extract = False
-			for id, char in enumerate(image_name):
-				if char == '.': break
-				if extract: extracted += char
-				if char == '!': 
-					extract = True
-					dis = len(image_name) - id
-			size = ["", ""]
-			id = 0
-			for char in extracted:
-				if char == 'x': id += 1
-				else: size[id] += char
-			size = [int(i) for i in size]
+		dis = 0
+		extracted = ''
+		extract = False
+		for id, char in enumerate(image_name):
+			if char == '.': break
+			if extract: extracted += char
+			if char == '!': 
+				extract = True
+				dis = len(image_name) - id
+		size = ["", ""]
+		id = 0
+		for char in extracted:
+			if char == 'x': id += 1
+			else: size[id] += char
+		size = [int(i) for i in size]
 
-			return size, dis
+		return size, dis
 
 
 	def unpack(self, tilemap, tilesize):
